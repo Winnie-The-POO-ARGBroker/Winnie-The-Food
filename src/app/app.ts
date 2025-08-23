131e9f9d5ca4eb6c61773cd078cf81ab0c84da6a
Jimenna <jimegalleguillo@gmail.com>
2025-08-22 23:02:25 -0300

    se resuelve conflicto app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ReactiveFormsModule],
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
