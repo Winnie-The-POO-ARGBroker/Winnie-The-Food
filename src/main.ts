7bb735934d285195407402e4b9df4c816bfc2a28
gigilvsarg <gigilavisse@gmail.com>
2025-10-13 19:33:35 -0300

    Carga de Pipe y modificacion de archivos app
    Se realiza la carga del pipe para la dificultad de las recetas y se modifican los archivos de app.ts app.routes y app.html

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
+import { httpLoaderInterceptor } from './app/core/http-loader.interceptor';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([httpLoaderInterceptor])),
   ]
 }).catch(console.error);
