4d30343966b2ada53da4ba75286709a297e120be
Rodrigo Valdez <fori2001@hotmail.com>
2025-10-13 22:42:57 -0300

    Carga de archivos para prueba de conexion de base de datos
    Carga de archivos para prueba de conexion de base de datos

--- api/api/api/wsgi.py
+++ api/api/api/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from core.check_db import check_database_connection
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
+
+if os.getenv("DEBUG", "False") == "True":
+    check_database_connection()
